2fe110cef6a6e31f8f9fecea4b5d1509ce490e45
Ivan Lee <leeivan1007@gmail.com>
2023-12-18 13:37:34 +0800

    Update schedule_cnn.sh

--- schedule_cnn.sh
+++ schedule_cnn.sh
@@ -2,8 +2,8 @@
 
 dataset="$1"
 NUM="$2"
+slight_disturbance="$3"
 
 for (( i=1; i<=${NUM}; i++ )); do
-    echo "i=${i}"
-    python train_cnn.py --dataset ${dataset}
+    python train_cnn.py --dataset ${dataset} ${slight_disturbance}
 done
